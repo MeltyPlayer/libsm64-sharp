63067e85633988208317fd8ffe66c793cd0a8176
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-13 20:50:09 -0500

    Set up basic implementations for the SM64 context and Mario.

---
+++ LibSm64Sharp/src/Sm64Context.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices;
+
+using libsm64sharp.lowlevel;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+
+namespace libsm64sharp {
+  public sealed partial class Sm64Context : ISm64Context {
+    private const int SM64_TEXTURE_WIDTH = 64 * 11;
+    private const int SM64_TEXTURE_HEIGHT = 64;
+
+    public Sm64Context(byte[] romBytes,
+                       out Image<Rgba32> image,
+                       Action<string> debugPrintCallback) {
+      var callbackDelegate = new DebugPrintFuncDelegate(debugPrintCallback);
+      var romHandle = GCHandle.Alloc(romBytes, GCHandleType.Pinned);
+      var textureData = new byte[4 * SM64_TEXTURE_WIDTH * SM64_TEXTURE_HEIGHT];
+      var textureDataHandle = GCHandle.Alloc(textureData, GCHandleType.Pinned);
+
+      LibSm64Interop.sm64_global_init(romHandle.AddrOfPinnedObject(),
+                                      textureDataHandle.AddrOfPinnedObject(),
+                                      Marshal.GetFunctionPointerForDelegate(
+                                          callbackDelegate));
+
+      image = new Image<Rgba32>(SM64_TEXTURE_WIDTH, SM64_TEXTURE_HEIGHT);
+      {
+        var frame = image.Frames[0];
+        for (var ix = 0; ix < SM64_TEXTURE_WIDTH; ix++) {
+          for (var iy = 0; iy < SM64_TEXTURE_HEIGHT; iy++) {
+            var pixel = frame[ix, iy];
+
+            var pixelOffset = 4 * (ix + SM64_TEXTURE_WIDTH * iy);
+            pixel.R = textureData[pixelOffset + 0];
+            pixel.G = textureData[pixelOffset + 1];
+            pixel.B = textureData[pixelOffset + 2];
+            pixel.A = textureData[pixelOffset + 3];
+
+            frame[ix, iy] = pixel;
+          }
+        }
+      }
+
+      romHandle.Free();
+      textureDataHandle.Free();
+    }
+
+    ~Sm64Context() {
+      this.ReleaseUnmanagedResources_();
+    }
+
+    public void Dispose() {
+      this.ReleaseUnmanagedResources_();
+      GC.SuppressFinalize(this);
+    }
+
+    private void ReleaseUnmanagedResources_()
+      => LibSm64Interop.sm64_global_terminate();
+  }
+}
---
+++ LibSm64Sharp/src/Sm64Mario.cs
@@ -0,0 +1,29 @@
+﻿using libsm64sharp.lowlevel;
+
+
+namespace libsm64sharp {
+  public partial class Sm64Context {
+    public ISm64Mario CreateMario(short x, short y, short z)
+      => new Sm64Mario(x, y, z);
+
+    private class Sm64Mario : ISm64Mario {
+      private readonly uint id_;
+
+      public Sm64Mario(short x, short y, short z) {
+        this.id_ = LibSm64Interop.sm64_mario_create(x, y, z);
+      }
+
+      ~Sm64Mario() {
+        this.ReleaseUnmanagedResources_();
+      }
+
+      public void Dispose() {
+        this.ReleaseUnmanagedResources_();
+        GC.SuppressFinalize(this);
+      }
+
+      private void ReleaseUnmanagedResources_()
+        => LibSm64Interop.sm64_mario_delete(this.id_);
+    }
+  }
+}
