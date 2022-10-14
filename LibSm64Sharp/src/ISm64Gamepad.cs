a4e44dcea48eb7b16465c9b87a667694bca4d7ce
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-13 23:01:13 -0500

    Set up an interface and type representing a Sm64 gamepad.

---
+++ LibSm64Sharp/src/ISm64Gamepad.cs
@@ -0,0 +1,20 @@
+﻿namespace libsm64sharp {
+  public interface ISm64Gamepad {
+    ISm64Vector2<float> AnalogStick { get; }
+    void ScheduleAnalogStick(float x, float y);
+
+    ISm64Vector2<float> CamLook { get; }
+    void ScheduleCamLook(float x, float y);
+
+    bool IsAButtonDown { get; }
+    void ScheduleAButton(bool isAButtonDown);
+
+    bool IsBButtonDown { get; }
+    void ScheduleBButton(bool isBButtonDown);
+
+    bool IsZButtonDown { get; }
+    void ScheduleZButton(bool isZButtonDown);
+   
+    void Tick();
+  }
+}
---
+++ LibSm64Sharp/src/Sm64Gamepad.cs
@@ -0,0 +1,69 @@
+﻿namespace libsm64sharp {
+  public partial class Sm64Context {
+    private class Sm64Gamepad : ISm64Gamepad {
+      private readonly Sm64Vector2<float> analogStick_ = new();
+      private Sm64Vector2<float>? scheduledAnalogStick_;
+
+      private readonly Sm64Vector2<float> camLook_ = new();
+      private Sm64Vector2<float>? scheduledCamLook_;
+
+      private bool? scheduledIsAButtonDown_;
+      private bool? scheduledIsBButtonDown_;
+      private bool? scheduledIsZButtonDown_;
+
+      public ISm64Vector2<float> AnalogStick => this.analogStick_;
+
+      public void ScheduleAnalogStick(float x, float y)
+        => this.scheduledAnalogStick_ = new Sm64Vector2<float> {X = x, Y = y};
+
+      public ISm64Vector2<float> CamLook => this.camLook_;
+
+      public void ScheduleCamLook(float x, float y)
+        => this.scheduledCamLook_ = new Sm64Vector2<float> {X = x, Y = y};
+
+      public bool IsAButtonDown { get; private set; }
+
+      public void ScheduleAButton(bool isAButtonDown)
+        => this.scheduledIsAButtonDown_ = isAButtonDown;
+
+      public bool IsBButtonDown { get; private set; }
+
+      public void ScheduleBButton(bool isBButtonDown)
+        => this.scheduledIsBButtonDown_ = isBButtonDown;
+
+      public bool IsZButtonDown { get; private set; }
+
+      public void ScheduleZButton(bool isZButtonDown)
+        => this.scheduledIsZButtonDown_ = isZButtonDown;
+
+      public void Tick() {
+        if (this.scheduledAnalogStick_ != null) {
+          this.analogStick_.X = this.scheduledAnalogStick_.X;
+          this.analogStick_.Y = this.scheduledAnalogStick_.Y;
+          this.scheduledAnalogStick_ = null;
+        }
+
+        if (this.scheduledCamLook_ != null) {
+          this.camLook_.X = this.scheduledCamLook_.X;
+          this.camLook_.Y = this.scheduledCamLook_.Y;
+          this.scheduledCamLook_ = null;
+        }
+
+        if (this.scheduledIsAButtonDown_ != null) {
+          this.IsAButtonDown = this.scheduledIsAButtonDown_.Value;
+          this.scheduledIsAButtonDown_ = null;
+        }
+
+        if (this.scheduledIsBButtonDown_ != null) {
+          this.IsBButtonDown = this.scheduledIsBButtonDown_.Value;
+          this.scheduledIsBButtonDown_ = null;
+        }
+
+        if (this.scheduledIsZButtonDown_ != null) {
+          this.IsZButtonDown = this.scheduledIsZButtonDown_.Value;
+          this.scheduledIsZButtonDown_ = null;
+        }
+      }
+    }
+  }
+}
