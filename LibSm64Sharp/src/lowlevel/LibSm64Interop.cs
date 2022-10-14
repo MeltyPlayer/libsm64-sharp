27a971af262180ed4b1ed5de02552ae6ce6e0fab
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-14 17:24:30 -0500

    Added support for running in both 32-bit and 64-bit mode.

--- LibSm64Sharp/src/lowlevel/LibSm64Interop.cs
+++ LibSm64Sharp/src/lowlevel/LibSm64Interop.cs
@@ -3,48 +3,52 @@
 
 namespace libsm64sharp.lowlevel {
   public static class LibSm64Interop {
-    private const string SM64_X86_DLL = "lib\\sm64-x86.dll";
+#if WIN64
+    private const string SM64_DLL = "lib\\sm64-x64.dll";
+#else
+    private const string SM64_DLL = "lib\\sm64-x86.dll";
+#endif
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern void sm64_global_init(
         IntPtr rom,
         IntPtr outTexture,
         IntPtr debugPrintFunctionPtr);
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern void sm64_global_terminate();
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern void sm64_static_surfaces_load(
         LowLevelSm64Surface[] surfaces,
         ulong numSurfaces);
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern uint sm64_mario_create(
         short marioX,
         short marioY,
         short marioZ);
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern void sm64_mario_tick(
         uint marioId,
         ref LowLevelSm64MarioInputs inputs,
         ref LowLevelSm64MarioOutState outState,
         ref LowLevelSm64MarioGeometryBuffers outBuffers);
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern void sm64_mario_delete(uint marioId);
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern uint sm64_surface_object_create(
         ref LowLevelSm64SurfaceObject surfaceObject);
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern void sm64_surface_object_move(
         uint objectId,
         ref LowLevelSm64ObjectTransform transform);
 
-    [DllImport(SM64_X86_DLL)]
+    [DllImport(SM64_DLL)]
     public static extern void sm64_surface_object_delete(uint objectId);
   }
 }
--- README.md
+++ README.md
@@ -6,8 +6,6 @@ This library provides both:
 - [libsm64-unity](https://github.com/libsm64/libsm64-unity)'s interop layer for calling libsm64 methods directly.
 - A C# object-oriented layer for safely calling the libsm64 methods.
 
-_Note: Currently, only a 32-bit DLL is included with this library._
-
 ## Credits
 
 - [libsm64](https://github.com/libsm64/libsm64), the original C library that provides this functionality.
