759e1905542f1d91b56d6b1a3324172f5247c189
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-13 22:54:51 -0500

    Set up types for tracking 2-number vectors.

---
+++ LibSm64Sharp/src/ISm64Vector2.cs
@@ -0,0 +1,9 @@
+﻿using System.Numerics;
+
+
+namespace libsm64sharp {
+  public interface ISm64Vector2<TNumber> where TNumber : INumber<TNumber> {
+    TNumber X { get; }
+    TNumber Y { get; }
+  }
+}
---
+++ LibSm64Sharp/src/Sm64Vector2.cs
@@ -0,0 +1,12 @@
+﻿using System.Numerics;
+
+
+namespace libsm64sharp {
+  public partial class Sm64Context {
+    private class Sm64Vector2<TNumber> : ISm64Vector2<TNumber>
+        where TNumber : INumber<TNumber> {
+      public TNumber X { get; set; }
+      public TNumber Y { get; set; }
+    }
+  }
+}
