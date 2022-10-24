bad1495982a6cd015ccbfbd251d4896e29ea9a55
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-24 17:02:36 -0500

    Set up structs for low-level SM64 audio data.

---
+++ LibSm64Sharp/src/lowlevel/AudioStructs.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace libsm64sharp.lowlevel {
+  [StructLayout(LayoutKind.Sequential)]
+  public struct LowLevelSm64AudioBanks {
+    public int numCtlEntries;
+    public IntPtr ctlEntries;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  public struct LowLevelSm64CtlEntry {
+    public byte unused;
+    public byte numInstruments;
+    public byte numDrums;
+    public IntPtr instruments;
+    public IntPtr drums;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  public struct LowLevelSm64Drum {
+    public byte releaseRate;
+    public byte pan;
+    public byte loaded;
+    public LowLevelSm64AudioBankSound sound;
+    public IntPtr envelope;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  public struct LowLevelSm64Instrument {
+    public byte loaded;
+    public byte normalRangeLo;
+    public byte normalRangeHi;
+    public byte releaseRate;
+    public IntPtr envelope;
+    public LowLevelSm64AudioBankSound lowNotesSound;
+    public LowLevelSm64AudioBankSound normalNotesSound;
+    public LowLevelSm64AudioBankSound highNotesSound;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  public struct LowLevelSm64AudioBankSound {
+    public IntPtr sample;
+    public float tuning;
+  }
+
+  [StructLayout(LayoutKind.Sequential)]
+  public struct LowLevelSm64AudioBankSample {
+    public byte unused;
+    public byte loaded;
+    public IntPtr sampleAddr;
+    public IntPtr loop;
+    public IntPtr book;
+    public uint sampleSize;
+  }
+}
