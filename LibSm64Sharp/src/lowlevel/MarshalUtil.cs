eb911ece325dae9d58205d0398317cb1e88c6a70
MeltyPlayer <mastergamepro@hotmail.com>
2022-10-24 17:01:40 -0500

    Added utils for marshalling data from low-level logic.

---
+++ LibSm64Sharp/src/lowlevel/MarshalUtil.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+
+namespace libsm64sharp.lowlevel {
+  public static class MarshalUtil {
+    public static T? MarshalRef_<T>(IntPtr ptr)
+      => ptr.ToInt64() != 0 ? Marshal.PtrToStructure<T>(ptr) : default;
+
+    public static T[] MarshalArray_<T>(IntPtr ptrToArray, int count) {
+      var size = Marshal.SizeOf<T>();
+
+      var array = new T?[count];
+      for (var i = 0; i < count; i++) {
+        var ptr = new IntPtr(ptrToArray.ToInt64() + i * size);
+        array[i] = MarshalRef_<T>(ptr);
+      }
+
+      return array;
+    }
+
+    public static T?[] MarshalArrayOfRefs_<T>(IntPtr ptrToArray, int count) {
+      var ptrSize = Marshal.SizeOf<IntPtr>();
+
+      var array = new T?[count];
+      for (var i = 0; i < count; i++) {
+        var ptrToPtr = new IntPtr(ptrToArray.ToInt64() + i * ptrSize);
+        var ptr = Marshal.PtrToStructure<IntPtr>(ptrToPtr);
+        array[i] = MarshalRef_<T>(ptr);
+      }
+
+      return array;
+    }
+  }
+}
