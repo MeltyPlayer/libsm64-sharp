ad65f5a05280a7103d5927c9d67ae5c087fbaecd
MeltyPlayer <mastergamepro@hotmail.com>
2022-11-05 01:23:43 -0500

    Pulled out more SM64 camera methods.

---
+++ Demo Project/src/camera/sm64/Sm64Camera_dist_and_angle.cs
@@ -0,0 +1,30 @@
+﻿using System.Numerics;
+
+namespace demo.camera.sm64 {
+  public partial class Sm64Camera {
+    /**
+     * Take the vector starting at 'from' pointed at 'to' an retrieve the length
+     * of that vector, as well as the yaw and pitch angles.
+     * Basically it converts the direction to spherical coordinates.
+     */
+    public void vec3f_get_dist_and_angle(Vector3 from, Vector3 to, out float dist, out short pitch, out short yaw) {
+      var x = to[0] - from[0];
+      var y = to[1] - from[1];
+      var z = to[2] - from[2];
+
+      dist = MathF.Sqrt(x * x + y * y + z * z);
+      pitch = atan2s(MathF.Sqrt(x * x + z * z), y);
+      yaw = atan2s(z, x);
+    }
+
+    /**
+     * Construct the 'to' point which is distance 'dist' away from the 'from' position,
+     * and has the angles pitch and yaw.
+     */
+    public void vec3f_set_dist_and_angle(Vector3 from, ref Vector3 to, float dist, short pitch, short yaw) {
+      to[0] = from[0] + dist * coss(pitch) * sins(yaw);
+      to[1] = from[1] + dist * sins(pitch);
+      to[2] = from[2] + dist * coss(pitch) * coss(yaw);
+    }
+  }
+}
--- Demo Project/src/camera/sm64/Sm64Camera_rotate.cs
+++ Demo Project/src/camera/sm64/Sm64Camera_rotate.cs
@@ -22,5 +22,30 @@ namespace demo.camera.sm64 {
       dst[1] = src[2] * sins(pitch) + src[1] * coss(pitch);
       dst[0] = src[0];
     }
+
+    /**
+     * Rotates a vector in the horizontal plane and copies it to a new vector.
+     */
+    public void rotatef_in_xz(ref Vector3 dst, Vector3 src, float yaw) {
+      yaw *= 180 / MathF.PI;
+
+      dst[0] = src[2] * MathF.Sin(yaw) + src[0] * MathF.Cos(yaw);
+      dst[1] = src[1];
+      dst[2] = src[2] * MathF.Cos(yaw) - src[0] * MathF.Sin(yaw);
+    }
+
+    /**
+     * Rotates a vector in the YZ plane and copies it to a new vector.
+     *
+     * Note: This function also flips the Z axis, so +Z moves forward, not backward like it would in world
+     * space. If possible, use vec3f_set_dist_and_angle()
+     */
+    public void rotatef_in_yz(ref Vector3 dst, Vector3 src, float pitch) {
+      pitch *= 180 / MathF.PI;
+
+      dst[2] = -(src[2] * MathF.Cos(pitch) - src[1] * MathF.Sin(pitch));
+      dst[1] = src[2] * MathF.Sin(pitch) + src[1] * MathF.Cos(pitch);
+      dst[0] = src[0];
+    }
   }
 }
