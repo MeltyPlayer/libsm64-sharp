657f8d66530a88407f298eb4f2f24e087ba8aea5
MeltyPlayer <mastergamepro@hotmail.com>
2022-11-04 23:47:41 -0500

    Pulled out camera_approach_float_symmetric_bool into a static helper function.

---
+++ Demo Project/src/camera/sm64/Sm64CameraHelpers.cs
@@ -0,0 +1,33 @@
+﻿namespace demo.camera.sm64 {
+  public class Sm64CameraHelpers {
+    /**
+     * Approaches a value by a given increment, returns FALSE if the target is
+     * reached. Appears to be a strange way of implementing
+     * approach_float_symmetric from object_helpers.c. It could possibly be an
+     * older version of the function
+     */
+    public static bool camera_approach_float_symmetric_bool(ref float current, float target, float increment) {
+      var dist = target - current;
+
+      if (increment < 0) {
+        increment = -1 * increment;
+      }
+      if (dist > 0) {
+        dist -= increment;
+        if (dist > 0) {
+          current = target - dist;
+        } else {
+          current = target;
+        }
+      } else {
+        dist += increment;
+        if (dist < 0) {
+          current = target - dist;
+        } else {
+          current = target;
+        }
+      }
+      return !(Math.Abs(current - target) < .001);
+    }
+  }
+}
