39b40e34f9f0bb219c9792663eb7bb122de676e4
MeltyPlayer <mastergamepro@hotmail.com>
2022-11-06 00:31:36 -0500

    Added some more types from SM64's camera logic.

---
+++ Demo Project/src/camera/sm64/Sm64Camera_camera.cs
@@ -0,0 +1,55 @@
+﻿namespace demo.camera.sm64 {
+  public partial class Sm64Camera {
+    private Camera gCamera;
+
+    /**
+     * The main camera struct. Gets updated by the active camera mode and the current level/area. In
+     * update_lakitu, its pos and focus are used to calculate lakitu's next position and focus, which are
+     * then used to render the game.
+     */
+    class Camera {
+      /*0x00*/
+      byte mode; // What type of mode the camera uses (see defines above)
+      /*0x01*/
+      byte defMode;
+      /**
+       * Determines what direction Mario moves in when the analog stick is moved.
+       *
+       * @warning This is NOT the camera's xz-rotation in world space. This is the angle calculated from the
+       *          camera's focus TO the camera's position, instead of the other way around like it should
+       *          be. It's effectively the opposite of the camera's actual yaw. Use
+       *          vec3f_get_dist_and_angle() if you need the camera's yaw.
+       */
+      /*0x02*/
+      short yaw;
+      /*0x04*/
+      Vec3f focus = new();
+      /*0x10*/
+      Vec3f pos = new();
+      /*0x1C*/
+      Vec3f unusedVec1 = new();
+      /// The x coordinate of the "center" of the area. The camera will rotate around this point.
+      /// For example, this is what makes the camera rotate around the hill in BoB
+      /*0x28*/
+      float areaCenX;
+      /// The z coordinate of the "center" of the area. The camera will rotate around this point.
+      /// For example, this is what makes the camera rotate around the hill in BoB
+      /*0x2C*/
+      float areaCenZ;
+      /*0x30*/
+      byte cutscene;
+      /*0x31*/
+      byte[] filler1 = new byte[8];
+      /*0x3A*/
+      short nextYaw;
+      /*0x3C*/
+      byte[] filler2 = new byte[40];
+      /*0x64*/
+      byte doorStatus;
+      /// The y coordinate of the "center" of the area. Unlike areaCenX and areaCenZ, this is only used
+      /// when paused. See zoom_out_if_paused_and_outside
+      /*0x68*/
+      float areaCenY;
+    }
+  }
+}
---
+++ Demo Project/src/camera/sm64/Sm64Camera_lakitu.cs
@@ -0,0 +1,135 @@
+﻿namespace demo.camera.sm64 {
+  public partial class Sm64Camera {
+    private LakituState gLakituState = new();
+
+    /**
+     * A struct containing info pertaining to lakitu, such as his position and focus, and what
+     * camera-related effects are happening to him, like camera shakes.
+     *
+     * This struct's pos and focus are what is actually used to render the game.
+     *
+     * @see update_lakitu()
+     */
+    class LakituState {
+      /**
+       * Lakitu's position, which (when CAM_FLAG_SMOOTH_MOVEMENT is set), approaches his goalPos every frame.
+       */
+      /*0x00*/
+      Vec3f curFocus = new();
+      /**
+       * Lakitu's focus, which (when CAM_FLAG_SMOOTH_MOVEMENT is set), approaches his goalFocus every frame.
+       */
+      /*0x0C*/
+      Vec3f curPos = new();
+      /**
+       * The focus point that lakitu turns towards every frame.
+       * If CAM_FLAG_SMOOTH_MOVEMENT is unset, this is the same as curFocus.
+       */
+      /*0x18*/
+      Vec3f goalFocus = new();
+      /**
+       * The point that lakitu flies towards every frame.
+       * If CAM_FLAG_SMOOTH_MOVEMENT is unset, this is the same as curPos.
+       */
+      /*0x24*/
+      Vec3f goalPos = new();
+
+      /*0x30*/
+      byte[] filler1 = new byte[12]; // extra unused Vec3f?
+
+      /// Copy of the active camera mode
+      /*0x3C*/
+      byte mode;
+      /// Copy of the default camera mode
+      /*0x3D*/
+      byte defMode;
+
+      /*0x3E*/
+      byte[] filler2 = new byte[10];
+
+      /*0x48*/
+      float focusDistance; // unused
+      /*0x4C*/
+      short oldPitch; // unused
+      /*0x4E*/
+      short oldYaw;   // unused
+      /*0x50*/
+      short oldRoll;  // unused
+
+      /// The angular offsets added to lakitu's pitch, yaw, and roll
+      /*0x52*/
+      Vec3s shakeMagnitude;
+
+      // shake pitch, yaw, and roll phase: The progression through the camera shake (a cosine wave).
+      // shake pitch, yaw, and roll vel: The speed of the camera shake.
+      // shake pitch, yaw, and roll decay: The shake's deceleration.
+      /*0x58*/
+      short shakePitchPhase;
+      /*0x5A*/
+      short shakePitchVel;
+      /*0x5C*/
+      short shakePitchDecay;
+
+      /*0x60*/
+      Vec3f unusedVec1 = new();
+      /*0x6C*/
+      Vec3s unusedVec2 = new();
+      /*0x72*/
+      byte[] filler3 = new byte[8];
+
+      /// Used to rotate the screen when rendering.
+      /*0x7A*/
+      short roll;
+      /// Copy of the camera's yaw.
+      /*0x7C*/
+      short yaw;
+      /// Copy of the camera's next yaw.
+      /*0x7E*/
+      short nextYaw;
+      /// The actual focus point the game uses to render.
+      /*0x80*/
+      Vec3f focus = new();
+      /// The actual position the game is rendered from.
+      /*0x8C*/
+      Vec3f pos = new();
+
+      // Shake variables: See above description
+      /*0x98*/
+      short shakeRollPhase;
+      /*0x9A*/
+      short shakeRollVel;
+      /*0x9C*/
+      short shakeRollDecay;
+      /*0x9E*/
+      short shakeYawPhase;
+      /*0xA0*/
+      short shakeYawVel;
+      /*0xA2*/
+      short shakeYawDecay;
+
+      // focH,Vspeed: how fast lakitu turns towards his goalFocus.
+      /// By default HSpeed is 0.8, so lakitu turns 80% of the horz distance to his goal each frame.
+      /*0xA4*/
+      float focHSpeed;
+      /// By default VSpeed is 0.3, so lakitu turns 30% of the vert distance to his goal each frame.
+      /*0xA8*/
+      float focVSpeed;
+
+      // posH,Vspeed: How fast lakitu flies towards his goalPos.
+      /// By default they are 0.3, so lakitu will fly 30% of the way towards his goal each frame.
+      /*0xAC*/
+      float posHSpeed;
+      /*0xB0*/
+      float posVSpeed;
+
+      /// The roll offset applied during part of the key dance cutscene
+      /*0xB4*/
+      short keyDanceRoll;
+      /// Mario's action from the previous frame. Only used to determine if Mario just finished a dive.
+      /*0xB8*/
+      uint lastFrameAction;
+      /*0xBC*/
+      short unused;
+    }
+  }
+}
