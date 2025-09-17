18fbf32592dda86215fade0fab3838d00a8bce63
MeltyPlayer <mastergamepro@hotmail.com>
2025-09-17 00:23:17 -0500

    Oops, some files were omitted from the .gitignore.

---
+++ LibSm64Sharp/src/DamageType.cs
@@ -0,0 +1,8 @@
+﻿namespace libsm64sharp;
+
+public enum DamageType : uint {
+  DELAY_INVINCIBILITY = 0x00000002,
+
+  // Used by Bowser, sets Mario's forward velocity to 40 on hit
+  BIG_KNOCKBACK = 0x00000008
+}
---
+++ LibSm64Sharp/src/impl/Sm64Mario_Health.cs
@@ -0,0 +1,21 @@
+﻿using libsm64sharp.lowlevel;
+
+using System.Numerics;
+
+
+namespace libsm64sharp;
+
+public partial class Sm64Context {
+  private partial class Sm64Mario : ISm64Mario {
+    public void Damage(uint damage,
+                       DamageType damageType,
+                       Vector3 position)
+      => LibSm64Interop.sm64_mario_take_damage(
+          this.id_, damage, damageType, position.X, position.Y, position.Z);
+
+    public void Heal(byte healCounter)
+      => LibSm64Interop.sm64_mario_heal(this.id_, healCounter);
+
+    public void Kill() => LibSm64Interop.sm64_mario_kill(this.id_);
+  }
+}
